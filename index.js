ce70c683b9ec8e60c7e522497a9bcea729f64829
Milkac-Design <milkac88@gmail.com>
2020-09-08 14:30:44 +0200

    install randomcolor and chalk library and wrote the basic project

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('');
+//console.log('hi');
+const chalk = require('chalk');
+let randomColor = require('randomcolor'); 
+let hueChoice = process.argv[2];
+let lumChoice = process.argv[3];
+
+if (hueChoice === 'red' || hueChoice === 'orange' || hueChoice === 'yellow' || hueChoice === 'green' || hueChoice === 'blue' 
+|| hueChoice === 'purple' || hueChoice === 'pink')
+{
+let color = randomColor(
+    {hue: hueChoice,
+     luminosity: lumChoice
+})
+ 
+let pattern = ` #################### \n #################### 
+ ##### ${color} ###### \n #################### \n ####################`
+
+ console.log(chalk.hex(color)(pattern));}
+ else {console.log('Invalid color, please choose one of the following: red, orange, yellow, green, blue, purple, pink')}
