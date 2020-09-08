31c178a6432a77914065fff9797328f3a91760d7
Milkac-Design <milkac88@gmail.com>
2020-09-08 15:52:23 +0200

    add hash box

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let randomColor = require('randomcolor');
 let hueChoice = process.argv[2];
 let lumChoice = process.argv[3];
 
+
+ let rows = process.argv[4];
+ let columns = process.argv[6]
+
+
 if (hueChoice === 'red' || hueChoice === 'orange' || hueChoice === 'yellow' || hueChoice === 'green' || hueChoice === 'blue' 
 || hueChoice === 'purple' || hueChoice === 'pink')
 {
@@ -15,5 +20,21 @@ let color = randomColor(
 let pattern = ` #################### \n #################### 
  ##### ${color} ###### \n #################### \n ####################`
 
- console.log(chalk.hex(color)(pattern));}
- else {console.log('Invalid color, please choose one of the following: red, orange, yellow, green, blue, purple, pink')}
+ let stretch 
+
+ console.log(chalk.hex(color)(pattern));
+ 
+
+
+
+  let star = "";
+for(let i=1; i<=rows; i++) {
+  
+  for(let j=1; j<=columns; j++) {
+    star += "#";
+  }
+  star += "\n";
+}
+console.log(chalk.hex(color)(star));}
+
+else {console.log('Invalid color, please choose one of the following: red, orange, yellow, green, blue, purple, pink')}
