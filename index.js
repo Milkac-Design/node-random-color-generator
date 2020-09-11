e1dfff43761e1acd9873c2da0d9025a5e3172c70
Milkac-Design <milkac88@gmail.com>
2020-09-11 14:18:01 +0200

    fix color to be random if no user input

--- index.js
+++ index.js
@@ -27,17 +27,25 @@ let pattern = ` #################### \n ####################
 
 
 
-  let star = "";
+  let hashtag = "";
 for(let i=1; i<=rows; i++) {
     if (i !== Math.ceil(rows/2)){
   
   for(let j=1; j<=columns; j++) {
-    star += "#";}
+    hashtag += "#";}
   } 
    if (i !== Math.ceil(rows/2)){
-      star += "\n";
-  }else {star += `##  ${color}  ## \n`}
+      hashtag += "\n";
+  }else {hashtag += `##  ${color}  ## \n`}
 }
-console.log(chalk.hex(color)(star));}
+console.log(chalk.hex(color)(hashtag));}
 
-else {console.log('Invalid color, please choose one of the following: red, orange, yellow, green, blue, purple, pink')}
+else {
+  let color = randomColor()
+   
+  let pattern = ` #################### \n #################### 
+ ##### ${color} ###### \n #################### \n #################### \n`
+  
+   let stretch 
+  
+   console.log(chalk.hex(color)(pattern));}
