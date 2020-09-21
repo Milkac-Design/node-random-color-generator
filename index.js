8986a5fb5868ca3a6ff4853c4bccfd92b83257df
Milkac-Design <milkac88@gmail.com>
2020-09-21 16:50:40 +0200

    Fix prettier to fix the code look

--- index.js
+++ index.js
@@ -4,46 +4,47 @@ const randomColor = require('randomcolor');
 const hueChoice = process.argv[2];
 const lumChoice = process.argv[3];
 
-
 const rows = process.argv[4];
-const columns = process.argv[6]
+const columns = process.argv[6];
 
-
-if (hueChoice === 'red' || hueChoice === 'orange' || hueChoice === 'yellow' || hueChoice === 'green' || hueChoice === 'blue'
-  || hueChoice === 'purple' || hueChoice === 'pink') {
-  let color = randomColor(
-    {
-      hue: hueChoice,
-      luminosity: lumChoice
-    })
+if (
+  hueChoice === 'red' ||
+  hueChoice === 'orange' ||
+  hueChoice === 'yellow' ||
+  hueChoice === 'green' ||
+  hueChoice === 'blue' ||
+  hueChoice === 'purple' ||
+  hueChoice === 'pink'
+) {
+  const color = randomColor({
+    hue: hueChoice,
+    luminosity: lumChoice,
+  });
 
   const pattern = ` #################### \n ####################
- ##### ${color} ###### \n #################### \n #################### \n`
-
+ ##### ${color} ###### \n #################### \n #################### \n`;
 
   console.log(chalk.hex(color)(pattern));
 
-
-  let hashtag = "";
+  let hashtag = '';
   for (let i = 1; i <= rows; i++) {
     if (i !== Math.ceil(rows / 2)) {
-
       for (let j = 1; j <= columns; j++) {
-        hashtag += "#";
+        hashtag += '#';
       }
     }
     if (i !== Math.ceil(rows / 2)) {
-      hashtag += "\n";
-    } else { hashtag += `##  ${color}  ## \n` }
+      hashtag += '\n';
+    } else {
+      hashtag += `##  ${color}  ## \n`;
+    }
   }
   console.log(chalk.hex(color)(hashtag));
-}
-
-else {
-  const color = randomColor()
+} else {
+  const color = randomColor();
 
   const pattern = ` #################### \n ####################
- ##### ${color} ###### \n #################### \n #################### \n`
+ ##### ${color} ###### \n #################### \n #################### \n`;
 
   console.log(chalk.hex(color)(pattern));
 }
