a4984e0f9ee50324f37106ddc92a7acf342ee7b1
Milkac-Design <milkac88@gmail.com>
2020-09-21 07:42:30 +0200

    Change let variables to const

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
 //console.log('hi');
 const chalk = require('chalk');
-let randomColor = require('randomcolor'); 
-let hueChoice = process.argv[2];
-let lumChoice = process.argv[3];
+const randomColor = require('randomcolor');
+const hueChoice = process.argv[2];
+const lumChoice = process.argv[3];
 
 
- let rows = process.argv[4];
- let columns = process.argv[6]
+const rows = process.argv[4];
+const columns = process.argv[6]
 
 
-if (hueChoice === 'red' || hueChoice === 'orange' || hueChoice === 'yellow' || hueChoice === 'green' || hueChoice === 'blue' 
-|| hueChoice === 'purple' || hueChoice === 'pink')
-{
-let color = randomColor(
-    {hue: hueChoice,
-     luminosity: lumChoice
-})
- 
-let pattern = ` #################### \n #################### 
+if (hueChoice === 'red' || hueChoice === 'orange' || hueChoice === 'yellow' || hueChoice === 'green' || hueChoice === 'blue'
+  || hueChoice === 'purple' || hueChoice === 'pink') {
+  let color = randomColor(
+    {
+      hue: hueChoice,
+      luminosity: lumChoice
+    })
+
+  const pattern = ` #################### \n ####################
  ##### ${color} ###### \n #################### \n #################### \n`
 
- let stretch 
 
- console.log(chalk.hex(color)(pattern));
- 
-
+  console.log(chalk.hex(color)(pattern));
 
 
   let hashtag = "";
-for(let i=1; i<=rows; i++) {
-    if (i !== Math.ceil(rows/2)){
-  
-  for(let j=1; j<=columns; j++) {
-    hashtag += "#";}
-  } 
-   if (i !== Math.ceil(rows/2)){
+  for (let i = 1; i <= rows; i++) {
+    if (i !== Math.ceil(rows / 2)) {
+
+      for (let j = 1; j <= columns; j++) {
+        hashtag += "#";
+      }
+    }
+    if (i !== Math.ceil(rows / 2)) {
       hashtag += "\n";
-  }else {hashtag += `##  ${color}  ## \n`}
+    } else { hashtag += `##  ${color}  ## \n` }
+  }
+  console.log(chalk.hex(color)(hashtag));
 }
-console.log(chalk.hex(color)(hashtag));}
 
 else {
-  let color = randomColor()
-   
-  let pattern = ` #################### \n #################### 
+  const color = randomColor()
+
+  const pattern = ` #################### \n ####################
  ##### ${color} ###### \n #################### \n #################### \n`
-  
-   let stretch 
-  
-   console.log(chalk.hex(color)(pattern));}
+
+  console.log(chalk.hex(color)(pattern));
+}
